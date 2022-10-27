4ae26661381bbbe0c15e3553a98feb857b8ca510
Cookkie92 <74307133+Cookkie92@users.noreply.github.com>
2022-10-27 22:24:51 +0200

    calling api and displaying

---
+++ filter.js
@@ -0,0 +1,29 @@
+const resultsContainer = document.querySelector(".result");
+const inputPrice = document.querySelector(".input-price");
+
+fetch("https://fakestoreapi.com/products")
+  .then((res) => res.json())
+  .then((jsonData) => {
+    try {
+      const results = jsonData;
+      console.log(results);
+      resultsContainer.innerHTML = "";
+      for (let i = 0; i < results[0].title.length; i++) {
+        resultsContainer.innerHTML += `
+                <div class="result">
+                <h3 class="title">${results[i].title}</h3>
+                <img class="image" src="${results[i].image}" alt="thumbnail">
+                <p>${results[i].price}$</p>
+                </div>
+            
+            `;
+        console.log(typeof results[i].price);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  });
+
+inputPrice.onkeyup = function () {
+  console.log(event.target.value);
+};
